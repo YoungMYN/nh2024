47724a1472704b30eb6eeb51cf32114fe2583712
YoungMYN <Shyliko315on@gmail.com>
2024-04-21 01:37:25 +0300

    Initial commit

---
+++ src/main/java/nuclear/hack/japp/JappApplication.java
@@ -0,0 +1,13 @@
+package nuclear.hack.japp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nuclear/hack/japp/JappApplicationTests.java
@@ -0,0 +1,13 @@
+package nuclear.hack.japp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
